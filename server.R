a87b578f25f47b897bc86f7b6a32902158db7dfd
Medernoe <noe.mederlet@univ-rouen.fr>
2025-11-05 14:04:41 +0100

    Ajout de l'option de téléchargement

--- server.R
+++ server.R
@@ -10,30 +10,53 @@ source("global.R")
 
 # Serveur
 server <- function(input, output) {
-  
+
+  #==============================================================================    
   #dataframe des pvalue et logFC
   data <- reactive({read.csv(input$file$datapath)})
   
-  #sortie volcanoplot
-  output$volcano_plot <- renderPlot({
+
+  #==============================================================================  
+  # Fonction réactive qui crée le plot
+  create_volcano <- reactive({
+    req(data())  # S'assure que les données existent
     
-    #transforme l'objet réactive de data en df 
     df <- data()
     
-    #fonction custom : script fonctions.R => seuil de significativité 
     df <- significativity(df, 
                           log2FC_cutoff = input$seuil_FC,
                           P_cutoff = input$seuil_pvalue)
     
-    #fonction custom : script fonctions.R => produit un V plot 
-    plot_volcano(df,
-                 log2FC_cutoff = input$seuil_FC,
-                 P_cutoff = input$seuil_pvalue,
-                 seuil_v = input$v,
-                 seuil_h = input$h,
-                 title = input$Titre)
+    plot_v <- plot_volcano(df,
+                           log2FC_cutoff = input$seuil_FC,
+                           P_cutoff = input$seuil_pvalue,
+                           seuil_v = input$v,
+                           seuil_h = input$h,
+                           title = input$Titre)
+    
+    return(plot_v)
   })
   
+  #==============================================================================  
+  #sortie volcanoplot
+  output$volcano_plot <- renderPlot({
+    create_volcano()
+  })
+  
+  #==============================================================================  
+  #Téléchargement du volcano plot
+  output$downloadVolcano <- downloadHandler(
+    filename = function() {
+      #creer le nom 
+      paste("Volcano_plot_", Sys.Date(), ".png", sep = "")
+    },
+    content = function(file) {
+      # Utiliser la fonction réactive
+      ggsave(file, plot = create_volcano(), width = 12, height = 8, dpi = 300)
+    }
+  )
+  
+  #==============================================================================  
   #sortie tableau
   output$data <- renderDataTable({
     df <- data()
